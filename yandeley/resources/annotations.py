2c15145d11ddfdf33a94da6c846afdd13f310b54
Shuichiro MAKIGAKI <shuichiro.makigaki@gmail.com>
2021-10-13 22:55:20 +0900

    Add support of annotation filter by document_id and group_id

--- yandeley/resources/annotations.py
+++ yandeley/resources/annotations.py
@@ -20,7 +20,7 @@ class Annotations(GetByIdResource, ListResource):
         """
         return super(Annotations, self).get(id)
 
-    def list(self, page_size=None, modified_since=None, deleted_since=None):
+    def list(self, document_id=None, group_id=None, page_size=None, modified_since=None, deleted_since=None):
         """
         Retrieves annotations as a paginated collection.
 
@@ -30,9 +30,9 @@ class Annotations(GetByIdResource, ListResource):
         :return: a :class:`Page <yandeley.pagination.Page>` of
         :class:`Annotations <yandeley.models.annotations.Annotation>`.
         """
-        return super(Annotations, self).list(page_size, modified_since=modified_since, deleted_since=deleted_since)
+        return super(Annotations, self).list(page_size, document_id=document_id, group_id=group_id, modified_since=modified_since, deleted_since=deleted_since)
 
-    def iter(self, page_size=None, modified_since=None, deleted_since=None):
+    def iter(self, document_id=None, group_id=None, page_size=None, modified_since=None, deleted_since=None):
         """
         Retrieves annotations that the logged-in user is a member of, as an iterator.
 
@@ -41,7 +41,7 @@ class Annotations(GetByIdResource, ListResource):
         :param deleted_since: if specified, only returns the annotations deleted after this timestamp.
         :return: an iterator of :class:`Annotations <yandeley.models.annotations.Annotation>`.
         """
-        return super(Annotations, self).iter(page_size, modified_since=modified_since, deleted_since=deleted_since)
+        return super(Annotations, self).iter(page_size, document_id=document_id, group_id=group_id, modified_since=modified_since, deleted_since=deleted_since)
 
     @property
     def _session(self):
